980b801552fb2161d5f5d5e9eb6c431179d2ecd3
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-12 18:02:09 +0900

    Title: 최대공약수 구하기, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240912/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,17 @@
+inp = input()
+arr = inp.split(" ")
+n = int(arr[0])
+m = int(arr[1])
+
+
+def max_num(n, m):
+    idx = 1
+    if n >= m: end = m
+    else: end = n
+    for i in range(1,end):
+        if n % i == 0 and m % i == 0:
+            idx = i
+        else: continue
+    print(idx)
+
+max_num(n,m)
