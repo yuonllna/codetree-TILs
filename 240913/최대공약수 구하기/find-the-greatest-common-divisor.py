cf87a47f60481d7876a54910edb0701d9fc96b1e
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-13 10:05:09 +0900

    Title: 최대공약수 구하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240913/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240913/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -5,7 +5,7 @@ m = int(arr[1])
 
 def gcd(n, m):
     ans = 1
-    for i in range(1, min(n, m)):
+    for i in range(1, min(n, m)+1):
         if(n % i == 0 and m % i == 0):
             ans = i
     print(ans)
