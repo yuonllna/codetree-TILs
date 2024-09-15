fff253c1ad69f445775eae1238c3d336d6f7f88b
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-15 11:52:31 +0900

    Title: 2021년 날짜의 유무, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/2021년 날짜의 유무/with-or-without-2021.py
+++ 240915/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,6 +1,7 @@
 m, d = map(int, input().split())
 
 def is_2021(m, d):
+    if m > 12: return False
     if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
         if d <= 31:
             return True
