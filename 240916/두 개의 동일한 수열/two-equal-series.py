bc1b0e56b2f344bc24d71ecc02befe2867ade036
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-16 00:46:15 +0900

    Title: 두 개의 동일한 수열, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/두 개의 동일한 수열/two-equal-series.py
+++ 240916/두 개의 동일한 수열/two-equal-series.py
@@ -3,6 +3,13 @@ n = int(input())
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
 
-if arr1.sort() == arr2.sort():
+arr1.sort()
+arr2.sort()
+cnt = 0
+for i in range(n):
+    if arr1[i] == arr2[i]:
+        cnt += 1
+
+if(cnt == n):
     print("Yes")
 else: print("No")
