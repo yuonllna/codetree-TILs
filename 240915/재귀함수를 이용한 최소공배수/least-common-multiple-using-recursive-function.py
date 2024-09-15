59c00ac12d5fc7062b4de20d1d35fc0f1b6e2e7e
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-15 21:24:17 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240915/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,17 +2,13 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 def gcd(a, b):
-    if a == 1 or b == 1:
-        return 1
-    res = 1
-    for i in range(2, min(a, b)+1):
-        if a % i == 0 and b % i == 0:
-            res = i
-    return res
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 def func(i):
     if i == 0:
-        return arr[0]
+        return arr[i]
     if gcd(arr[i], arr[i-1]) != 1:
         if i == 1:
             return gcd(arr[i], arr[i-1])
