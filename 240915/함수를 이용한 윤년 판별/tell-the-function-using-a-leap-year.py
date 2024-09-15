25a53b02d424ed3adb2c187cc8e1be8287fc89fe
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-15 11:07:16 +0900

    Title: 함수를 이용한 윤년 판별, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240915/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -6,7 +6,8 @@ def is_four(y):
 
 def is_hundred(y):
     if y % 100 == 0 and y % 400 != 0:
-        return True
+        return False
+    else: return True
 
 if is_four(y) or is_hundred(y):
     print("true")
