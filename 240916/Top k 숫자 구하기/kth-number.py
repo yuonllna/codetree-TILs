984ebe4ebe4eb750359a08d9ce06854b07dfe691
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-16 00:42:59 +0900

    Title: Top k 숫자 구하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/Top k 숫자 구하기/kth-number.py
+++ 240916/Top k 숫자 구하기/kth-number.py
@@ -2,8 +2,5 @@ n, k = map(int, input().split())
 
 arr = list(map(int, input().split()))
 
-if 0 <= k < n:  # k가 0 이상 n보다 작은지 확인
-    arr.sort()
-    print(arr[k])
-else:
-    print("Invalid index")
+arr.sort()
+print(arr[k-1])
