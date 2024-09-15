85758103e919f41412e23073033cb2d747def557
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-15 20:58:08 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240915/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def gcd(a, b):
+    if a == 1 or b == 1:
+        return 1
+    for i in range(2, min(a, b)+1):
+        if a % i == 0 and b % i == 0:
+            return i
+    return 1
+
+def func(i):
+    if i == 0:
+        return arr[0]
+    if gcd(arr[i], arr[i-1]) != 1:
+        return gcd(arr[i], arr[i-1]) * func(i-2)
+    if gcd(arr[i], arr[i-1]) == 1:
+        return arr[i] * func(i-1)
+
+print(func(n-1))
