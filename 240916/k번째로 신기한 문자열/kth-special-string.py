1d071ec46a967022ca73aa5f258bd4868915434d
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-16 01:02:33 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240916/k번째로 신기한 문자열/kth-special-string.py
+++ 240916/k번째로 신기한 문자열/kth-special-string.py
@@ -2,18 +2,22 @@ s = input()
 arr = s.split(" ")
 n = int(arr[0])
 k = int(arr[1])
-ap = arr[2]
+t = arr[2]
 
 arr = [] 
 for i in range(n):
     arr.append(input())
 
-cnt = 0
+cnt1 = 0
 arr.sort()
 for i in range(n):
-    if arr[i][0] == 'a' and arr[i][1] == 'p':
-        cnt += 1
-        if(cnt == k): 
+    cnt2 = 0
+    for j in range(len(t)):
+        if arr[i][j] == t[j]:
+            cnt2 += 1
+    if cnt2 == len(t):
+        cnt1 += 1
+        if(cnt1 == k): 
             print(arr[i])
             break
     else: continue
