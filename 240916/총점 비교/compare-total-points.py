eb45d874824f9743178ab47966240d9178defa78
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-16 23:00:27 +0900

    Title: 총점 비교, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240916/총점 비교/compare-total-points.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+n = int(input())
+students = []
+for i in range(n):
+    name, kor, eng, math = tuple(input().split())
+    students.append(Student(name, int(kor), int(eng), int(math)))
+
+
+students.sort(key=lambda x: (x.kor+x.eng+x.math))
+
+for student in students: 
+    print(student.name, student.kor, student.eng, student.math)
