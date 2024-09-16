6859faa542774a417907ca183211843467ebe98f
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-16 22:59:12 +0900

    Title: 국영수 순이지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240916/국영수 순이지/korean-english-math-order.py
+++ 240916/국영수 순이지/korean-english-math-order.py
@@ -12,7 +12,7 @@ for i in range(n):
     students.append(Student(name, kor, eng, math))
 
 
-students.sort(key=lambda x: (x.kor, x.eng, x.math), reverse=True)
+students.sort(key=lambda x: (-x.kor, -x.eng, -x.math))
 
 for student in students: 
     print(student.name, student.kor, student.eng, student.math)
