845da1ff1ef419ada077c9180605955136d3039b
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-16 16:18:05 +0900

    Title: k번째로 신기한 문자열, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240916/k번째로 신기한 문자열/kth-special-string.py
+++ 240916/k번째로 신기한 문자열/kth-special-string.py
@@ -4,20 +4,20 @@ n = int(arr[0])
 k = int(arr[1])
 t = arr[2]
 
+def is_same(str1, str2):
+    for j in range(len(t)):
+        if arr[i][j] != t[j]:
+            return False
+    return True
+
 arr = [] 
 for i in range(n):
     arr.append(input())
 
-cnt1 = 0
 arr.sort()
+cnt = 0
 for i in range(n):
-    cnt2 = 0
-    for j in range(0,len(t)):
-        if arr[i][j] == t[j]:
-            cnt2 += 1
-    if cnt2 == len(t):
-        cnt1 += 1
-        if(cnt1 == k): 
+    if is_same(arr[i], t):
+        cnt += 1
+        if cnt == k:
             print(arr[i])
-            break
-    else: continue
