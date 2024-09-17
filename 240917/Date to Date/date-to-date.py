878f202ed5c78c5a3b3ae57fca16113ea98547cf
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-17 16:27:12 +0900

    Title: Date to Date, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240917/Date to Date/date-to-date.py
+++ 240917/Date to Date/date-to-date.py
@@ -5,7 +5,11 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 month = m2 - m1
 
 if month == 0:
-    print(d2 - d1)
+    if d1 == d2:
+        print(1)
+    else: print(d2 - d1)
+    
+    
 else:
     sum = num_of_days[m1-1] - d1 + d2
     for i in range(m1, m2-1):
