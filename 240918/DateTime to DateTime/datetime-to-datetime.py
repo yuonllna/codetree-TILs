6437d37756836548c6a2a4089118414596854c4b
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-18 19:19:16 +0900

    Title: DateTime to DateTime, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240918/DateTime to DateTime/datetime-to-datetime.py
+++ 240918/DateTime to DateTime/datetime-to-datetime.py
@@ -2,17 +2,17 @@ a, b, c = tuple(map(int, input().split()))
 
 if a < 11 or b < 11 or b == 11 and c < 11:
     print(-1)
+else: 
+    day = a - 11
+    hour = b - 11
+    minute = c - 11
 
-day = a - 11
-hour = b - 11
-minute = c - 11
+    if hour < 0:
+        day -= 1
+        hour += 24
+    if minute < 0:
+        hour -= 1
+        minute += 60
 
-if hour < 0:
-    day -= 1
-    hour += 24
-if minute < 0:
-    hour -= 1
-    minute += 60
-
-ans = day * 24 * 60 + hour * 60 + minute
-print(ans)
+    ans = day * 24 * 60 + hour * 60 + minute
+    print(ans)
