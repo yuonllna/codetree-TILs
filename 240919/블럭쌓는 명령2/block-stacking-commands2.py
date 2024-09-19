22a43b75ab64ad172cd11c240f27b0a6ef881732
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-19 16:14:35 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240919/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240919/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,11 +3,11 @@ n, k = map(int, input().split())
 block = []
 for i in range(n):
     block.append(0)
-    
+
 for _ in range(k):
     start, end = map(int, input().split())
-    for i in range(start, end + 1):
-        block[i] += 1
+    for j in range(start, end + 1):
+        block[j] += 1
 
 max = 0
 for b in block:
