e12abb60b7f83e45e2c75037a6e6517be267f162
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-20 10:25:33 +0900

    Title: 최대로 겹치는 구간, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240920/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240920/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -6,7 +6,7 @@ for i in range(0, 201):
 
 for _ in range(n):
     x1, x2 = map(int, input().split())
-    for j in range(x1 + 100, x2 + 100 + 1):
+    for j in range(x1 + 100, x2 + 100):
         block[j] += 1
 
 max = 0
