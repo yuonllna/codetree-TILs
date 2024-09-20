c801c787531c1121b5f7cad4f3c4decf7e69a675
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-20 15:39:45 +0900

    Title: 왔다 갔던 구역 2, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240920/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240920/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -4,22 +4,22 @@ block = []
 for i in range(0, 2001):
     block.append(0)
 
-idx = 1000
+cur = 1000
 for _ in range(n):
     x, d = tuple(input().split())
     x = int(x)
     if d == "R":
-        for j in range(idx, idx + x + 1):
-            block[j] += 1        
+        end = cur + x
+        for j in range(cur, end):
+            block[j] += 1      
     else:
-        for j in range(idx, idx - x - 1, -1):
+        end = cur - x
+        for j in range(cur, end, -1):
             block[j] += 1
-    idx += x
-    
+    cur = end 
 
 sum = 0
 for i in range(len(block)):
-    if block[i] == 0: continue
     if block[i] >= 2:
         sum += 1
 
