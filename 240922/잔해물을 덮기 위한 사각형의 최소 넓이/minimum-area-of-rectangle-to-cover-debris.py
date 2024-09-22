9504f92d6d8c6eea2533fa0b980034db61dbab05
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-22 13:39:49 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 155ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -2,38 +2,31 @@ offset = 1000
 arr = [[0] * 2001 for _ in range(2001)]
 
 x1, y1, x2, y2 = tuple(map(int,input().split()))
-
-xmin, ymin, xmax, ymax = x1, y1, x2, y2
-for i in range(xmin + offset, xmax + offset + 1):
-    for j in range(ymin + offset, ymax + offset + 1):
+for i in range(x1 + offset, x2 + offset):
+    for j in range(y1 + offset, y2 + offset):
         arr[i][j] += 1
-
 
 x3, y3, x4, y4 = tuple(map(int,input().split()))
 for i in range(x3 + offset, x4 + offset):
     for j in range(y3 + offset, y4 + offset):
         if arr[i][j] == 1:
-            if xmin > i - offset:
-                xmin = i
-            if xmax < i - offset:
-                xmax = i
-            if ymin > j - offset:
-                ymin = j
-            if ymax < j - offset:
-                ymax = j
             arr[i][j] = 0
 
+xmin, ymin, xmax, ymax = x1, y1, x2, y2
+for i in range(0, 2001):
+    for j in range(0, 2001):
+        if arr[i][j] == 1:
+            if xmin > i - offset:
+                xmin = i - offset
+            if xmax < i - offset:
+                xmax = i - offset
+            if ymin > j - offset:
+                ymin = j - offset
+            if ymax < j - offset:
+                ymax = j - offset
+            
 sum = 0
-if arr[xmin + offset][ymin + offset] == 0 and arr[xmax + offset][ymax + offset] == 0:
-    print(0)
-elif arr[xmin + offset][ymin + offset] == 1 and arr[xmax + offset][ymax + offset] == 1:
-    for i in range(xmin + offset, xmax + offset):
-        for j in range(ymin + offset, ymax + offset):
-            sum += 1
-    print(sum)
-else:
-    for i in range(xmin + offset, xmax + offset):
-        for j in range(ymin + offset, ymax + offset):
-            if arr[i][j] == 1:
-                sum += 1   
-    print(sum)
+for i in range(xmin + offset, xmax + offset):
+    for j in range(ymin + offset, ymax + offset):
+        sum += 1   
+print(sum)
