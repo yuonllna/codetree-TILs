59b542bceed89530a09bdb7bf32e97c73025d2a4
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-22 12:41:53 +0900

    Title: 흰검 칠하기, Time: 192ms, Memory: 35MB, Status: Passed - CodeTree

--- 240922/흰검 칠하기/painting-white-black.py
+++ 240922/흰검 칠하기/painting-white-black.py
@@ -3,13 +3,13 @@ n = int(input())
 black = []
 white = []
 flag = []
-for _ in range(0, 100002):
+for _ in range(0, 200000):
     black.append(0)
 
-for _ in range(0, 100002):
+for _ in range(0, 200000):
     white.append(0)
 
-for _ in range(0, 100002):
+for _ in range(0, 200000):
     flag.append(None)
 
 cur = 50000
@@ -30,7 +30,7 @@ for _ in range(n):
 black_n = 0
 white_n = 0
 grey_n = 0
-for i in range(0, 100001):
+for i in range(0, 200000):
     if black[i] >= 2 and white[i] >= 2:
         grey_n += 1
     elif flag[i] == True:
