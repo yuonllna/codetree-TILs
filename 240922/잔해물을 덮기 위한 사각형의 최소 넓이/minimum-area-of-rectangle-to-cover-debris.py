04fe72982d9b32b5577bbbbb364d69181fdffd56
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-22 13:27:41 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 189ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240922/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -3,10 +3,11 @@ arr = [[0] * 2001 for _ in range(2001)]
 
 x1, y1, x2, y2 = tuple(map(int,input().split()))
 
-for i in range(x1 + offset, x2 + offset):
-    for j in range(y1 + offset, y2 + offset):
-        arr[i][j] += 1
 xmin, ymin, xmax, ymax = x1, y1, x2, y2
+for i in range(xmin + offset, xmax + offset + 1):
+    for j in range(ymin + offset, ymax + offset + 1):
+        arr[i][j] += 1
+
 
 x3, y3, x4, y4 = tuple(map(int,input().split()))
 for i in range(x3 + offset, x4 + offset):
@@ -22,12 +23,17 @@ for i in range(x3 + offset, x4 + offset):
                 ymax = j
             arr[i][j] = 0
 
-print(xmin, xmax, ymin, ymax)
+sum = 0
 if arr[xmin + offset][ymin + offset] == 0 and arr[xmax + offset][ymax + offset] == 0:
     print(0)
-else:
-    sum = 0
+elif arr[xmin + offset][ymin + offset] == 1 and arr[xmax + offset][ymax + offset] == 1:
     for i in range(xmin + offset, xmax + offset):
         for j in range(ymin + offset, ymax + offset):
             sum += 1
+    print(sum)
+else:
+    for i in range(xmin + offset, xmax + offset):
+        for j in range(ymin + offset, ymax + offset):
+            if arr[i][j] == 1:
+                sum += 1   
     print(sum)
