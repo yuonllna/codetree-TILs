bdb357d84289b2cd5e7d41de8e36a3874087eb27
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-23 22:36:50 +0900

    Title: 연속되는 수 4, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240923/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,19 @@
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+	int(input())
+	for _ in range(n)
+]
+
+ans, cnt = 0, 0
+for i in range(n):
+	# Case 1
+	if i >= 1 and arr[i] - arr[i-1] > 0:
+		cnt += 1
+	# Case 2
+	else:
+		cnt = 1
+	
+	ans = max(ans, cnt)
+
+print(ans)
