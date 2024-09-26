c5620aa06c544870fbceaeda3626fbda56de95a6
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-26 23:29:02 +0900

    Title: 선두를 지켜라, Time: 153ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240926/선두를 지켜라/keep-the-lead.py
+++ 240926/선두를 지켜라/keep-the-lead.py
@@ -17,22 +17,13 @@ for _ in range(m):
         pos_b[time_b] = pos_b[time_b - 1] + v
         time_b += 1
 
-winner = 0
-if pos_a[1] > pos_b[1]:
-    winner = 1
-elif pos_a[1] < pos_b[1]:
-    winner = 2
 
 sum = 0
-
-for i in range(2, min(time_a, time_b)):  
-    if pos_a[i] > pos_b[i] and pos_a[i-1] <= pos_b[i-1]:  
-        if winner != 1:
-            sum += 1
-        winner = 1  
-    elif pos_a[i] < pos_b[i] and pos_a[i-1] >= pos_b[i-1]: 
-        if winner != 2: 
-            sum += 1
-        winner = 2 
+winner = (pos_a[1] if pos_a[1] > pos_b[1] else pos_b[1])
+for i in range(2, time_a):
+    if pos_a[i] > pos_b[i] and pos_a[i-1] <= pos_b[i-1]:
+        sum += 1
+    elif pos_a[i] < pos_b[i] and pos_a[i-1] >= pos_b[i-1]:
+        sum += 1
 
 print(sum)
