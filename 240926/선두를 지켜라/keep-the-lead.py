3ef166871f398d91576d2c59e11a20b1af3b2c2d
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-26 16:06:53 +0900

    Title: 선두를 지켜라, Time: 170ms, Memory: 177MB, Status: Wrong Answer - CodeTree

--- 240926/선두를 지켜라/keep-the-lead.py
+++ 240926/선두를 지켜라/keep-the-lead.py
@@ -7,22 +7,21 @@ time_a = 1
 for _ in range(n):
     v, t = tuple(map(int, input().split()))
     for i in range(t):
-        pos_a[time_a] = v * time_a
+        pos_a[time_a] = pos_a[time_a - 1] + v
         time_a += 1
 
 time_b = 1
 for _ in range(m):
     v, t = tuple(map(int, input().split()))
     for _ in range(t):
-        pos_b[time_b] = v * time_b
+        pos_b[time_b] = pos_b[time_b - 1] + v
         time_b += 1
 
 sum = 0
-winner = (pos_a[1] if pos_a[1] > pos_b[1] else pos_b[1])
-for i in range(2, time_a):
-    if pos_a[i] > pos_b[i] and pos_a[i-1] < pos_b[i-1]:
+for i in range(2, min(time_a, time_b)):
+    if pos_a[i] > pos_b[i] and pos_a[i-1] <= pos_b[i-1]:
         sum += 1
-    elif pos_a[i] < pos_b[i] and pos_a[i-1] > pos_b[i-1]:
+    elif pos_a[i] < pos_b[i] and pos_a[i-1] >= pos_b[i-1]:
         sum += 1
 
 print(sum)
