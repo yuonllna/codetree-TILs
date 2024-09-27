3d6e1bc13fbbe3d70f01db8ce04a3276ff1473ab
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-27 09:55:21 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240927/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,35 @@
+MAX_T = 1000000
+
+n, m = tuple(map(int, input().split()))
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
+
+time_a = 1
+for _ in range(n):
+    t, d = tuple(input().split())
+    for _ in range(int(t)):
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+        time_a += 1
+
+time_b = 1
+for _ in range(m):
+    t, d = tuple(input().split())
+    for _ in range(int(t)):
+        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
+        time_b += 1
+
+ans = 0
+
+if time_a > time_b:
+	for i in range(time_b, time_a + 1):
+		pos_b[i] = pos_b[i - 1]
+elif time_a < time_b:
+	for i in range(time_a, time_b + 1):
+		pos_a[i] = pos_a[i - 1]
+
+for i in range(0, max(time_a, time_b)):
+    if pos_a[i] == pos_b[i] and pos_a[i-1] < pos_b[i-1]:
+        ans += 1
+    elif pos_a[i] == pos_b[i] and pos_a[i-1] > pos_b[i-1]:
+        ans += 1
+        
+print(ans)
