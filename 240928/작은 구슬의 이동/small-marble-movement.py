6e80da6618f7198d9eeef365b15406838fb3c41d
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-28 10:08:16 +0900

    Title: 작은 구슬의 이동, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240928/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,26 @@
+n, t = tuple(map(int, input().split()))
+r, c, d = tuple(input().split())
+x = int(r)
+y = int(c)
+arr = [[0] * (n+1) for _ in range(n+1)]
+
+dxs = [1, 0,  0, -1]
+dys = [0, 1, -1, 0]
+
+if d == "U":
+    dir_num = 0
+elif d == "R":
+    dir_num = 1
+elif d == "L":
+    dir_num = 2
+else: dir_num = 3
+
+for _ in range(t-1):
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+    if 1 <= nx <= n and 1 <= ny <= n:
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+    else:
+        dir_num = 3 - dir_num 
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+
+print(x, y)
