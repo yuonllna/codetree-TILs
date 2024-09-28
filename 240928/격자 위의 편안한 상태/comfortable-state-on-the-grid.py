1493ec5659652d3fee804e44bf2f297406825fd8
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-28 11:11:29 +0900

    Title: 격자 위의 편안한 상태, Time: 186ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240928/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240928/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -4,14 +4,16 @@ arr = [[0] * n for _ in range(n)]
 cnt = 0
 for _ in range(m):
     r, c = tuple(map(int, input().split()))
-    arr[r-1][c-1] = 1
-    if r-2 >= 0 and arr[r-2][c-1] == 1:
+    r -= 1
+    c -= 1
+    arr[r][c] = 1
+    if r-1 >= 0 and arr[r-1][c] == 1:
         cnt += 1
-    if r < n and arr[r][c-1] == 1:
+    if r+1 < n and arr[r+1][c] == 1:
         cnt += 1
-    if c-2 >= 0 and arr[r-1][c-2] == 1:
+    if c-1 >= 0 and arr[r][c-1] == 1:
         cnt += 1
-    if c < n and arr[r-1][c] == 1:
+    if c+1 < n and arr[r][c+1] == 1:
         cnt += 1
     if cnt >= 3:
         print(1)
