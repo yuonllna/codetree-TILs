63ee4d9face8b71fcadcb9315ebe8e0f0236f0d1
Yeonwoo Kim <98378283+yuonllna@users.noreply.github.com>
2024-09-29 20:15:11 +0900

    Title: 모이자, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240929/모이자/gather.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+min_sum = 10000
+for i in range(n):
+    loc = i
+    sum_diff = 0
+    for j in range(n):
+        sum_diff += arr[j] * abs(j - loc)
+    min_sum = min(min_sum, sum_diff)
+
+print(min_sum)
